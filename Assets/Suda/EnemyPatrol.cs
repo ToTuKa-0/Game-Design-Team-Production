4acfffbb73705deff6d6194084fd6c87ea4eedaf
hha332990 <hha332990@gmail.com>
2025-11-14 15:16:59 +0900

    Enemyが消えることなくなった

--- Assets/Suda/EnemyPatrol.cs
+++ Assets/Suda/EnemyPatrol.cs
@@ -11,6 +11,9 @@ public class EnemyPatrol : MonoBehaviour
     [Header("目的地への許容距離")]
     public float reachDistance = 0.1f;
 
+    [Header("Z座標固定")]
+    public float fixedZ = -1f; 
+
     private int currentIndex = 0;
 
     void Update()
@@ -19,18 +22,19 @@ public class EnemyPatrol : MonoBehaviour
 
         Transform target = movePoints[currentIndex];
 
-        transform.position = Vector2.MoveTowards(
+        Vector3 newPos = Vector2.MoveTowards(
             transform.position,
             target.position,
             speed * Time.deltaTime
         );
+        newPos.z = fixedZ; 
+        transform.position = newPos;
 
         float dist = Vector2.Distance(transform.position, target.position);
 
         if (dist < reachDistance)
         {
             currentIndex++;
-
             if (currentIndex >= movePoints.Length)
             {
                 currentIndex = 0;
